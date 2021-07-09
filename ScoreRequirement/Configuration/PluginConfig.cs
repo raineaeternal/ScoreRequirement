16b7c48f072bffd3a8e73535edec52237acdfb15
dawnvt <64136899+dawnvt@users.noreply.github.com>
2021-07-09 19:20:45 +0200

    Add: isComboBreakLimitEnabled as Bool Change: accRequirement from int to float

--- ScoreRequirement/Configuration/PluginConfig.cs
+++ ScoreRequirement/Configuration/PluginConfig.cs
@@ -11,8 +11,10 @@ namespace ScoreRequirement.Configuration
         public virtual bool isComboRequirementEnabled { get; set; }
         public virtual bool isPauseLimitEnabled { get; set; }
         public virtual bool isMissLimitEnabled { get; set; }
+        public virtual bool isComboBreakLimitEnabled { get; set; }
         public virtual int minimumComboCount { get; set; }
         public virtual int comboBreakLimit { get; set; }
+        public virtual float accRequirement { get; set; }
         public virtual int pauseLimit { get; set; }
         public virtual int missLimit { get; set; }
     }
