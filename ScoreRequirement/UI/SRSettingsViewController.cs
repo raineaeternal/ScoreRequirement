a3af131b6b85c2354e560bb288367bbe025a7834
dawnvt <64136899+dawnvt@users.noreply.github.com>
2021-07-09 19:41:08 +0200

    forgot this lol

--- ScoreRequirement/UI/SRSettingsViewController.cs
+++ ScoreRequirement/UI/SRSettingsViewController.cs
@@ -34,5 +34,75 @@ namespace ScoreRequirement.UI
             get => _config.isSREnabled;
             set => _config.isSREnabled = value;
         }
+        
+        [UIValue("comboRequirement")]
+        private bool ComboReqEnabled
+        {
+            get => _config.isComboRequirementEnabled;
+            set => _config.isComboRequirementEnabled = value;
+        }
+        
+        [UIValue("breakLimit")]
+        private bool BreakLimitEnabled
+        {
+            get => _config.isComboBreakLimitEnabled;
+            set => _config.isComboBreakLimitEnabled = value;
+        }
+        
+        [UIValue("accRequirement")]
+        private bool AccReqEnabled
+        {
+            get => _config.isAccRequirementEnabled;
+            set => _config.isAccRequirementEnabled = value;
+        }
+        
+        [UIValue("pauseLimit")]
+        private bool PauseLimitEnabled
+        {
+            get => _config.isPauseLimitEnabled;
+            set => _config.isPauseLimitEnabled = value;
+        }
+        
+        [UIValue("missCount")]
+        private bool MissCountEnabled
+        {
+            get => _config.isMissLimitEnabled;
+            set => _config.isMissLimitEnabled = value;
+        }
+        
+        [UIValue("comboRequirementSlider")]
+        private int ComboRequirement
+        {
+            get => _config.minimumComboCount;
+            set => _config.minimumComboCount = value;
+        }
+        
+        [UIValue("comboBreakLimitSlider")]
+        private int ComboBreakLimit
+        {
+            get => _config.comboBreakLimit;
+            set => _config.comboBreakLimit = value;
+        }
+        
+        [UIValue("accRequirementSlider")]
+        private float AccRequirement
+        {
+            get => _config.accRequirement;
+            set => _config.accRequirement = value;
+        }
+        
+        [UIValue("pauseLimitSlider")]
+        private int PauseLimit
+        {
+            get => _config.pauseLimit;
+            set => _config.pauseLimit = value;
+        }
+        
+        [UIValue("maxMissCountSlider")]
+        private int MaxMissCount
+        {
+            get => _config.missLimit;
+            set => _config.missLimit = value;
+        }
     }
 }
