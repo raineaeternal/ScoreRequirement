a8dd195515c704fd04e902af6169efac7586072e
dawnvt <64136899+dawnvt@users.noreply.github.com>
2021-07-10 03:26:12 +0200

    Add Class: AccuracyCheckManager.cs

---
+++ ScoreRequirement/Managers/AccuracyCheckManager.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using ScoreRequirement.Configuration;
+using UnityEngine;
+using Zenject;
+using SiraUtil.Services;
+
+namespace ScoreRequirement.Managers
+{
+    public class AccuracyCheckManager : MonoBehaviour, IInitializable
+    {
+        private AudioTimeSyncController _audioTimeSyncController;
+        private RelativeScoreAndImmediateRankCounter _relativeScoreAndImmediateRankCounter;
+        private PluginConfig _config;
+        private Submission _submission;
+
+
+        [Inject]
+        public void Construct(AudioTimeSyncController audioTimeSyncController, RelativeScoreAndImmediateRankCounter relativeScoreAndImmediateRankCounter, PluginConfig config, Submission submission)
+        {
+            _config = config;
+            _audioTimeSyncController = audioTimeSyncController;
+            _relativeScoreAndImmediateRankCounter = relativeScoreAndImmediateRankCounter;
+            _submission = submission;
+        }
+
+        public void Initialize()
+        {
+            
+        }
+
+        public void Update()
+        {
+            if (this._audioTimeSyncController.songTime >= this._audioTimeSyncController.songEndTime - 0.3f) return;
+            if (_relativeScoreAndImmediateRankCounter.relativeScore < _config.accRequirement) 
+                _submission?.DisableScoreSubmission("ScoreRequirement", "Accuracy was too low");
+           
+        }
+    }
+}
